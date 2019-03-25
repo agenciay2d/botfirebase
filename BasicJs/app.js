b63bb50b7c2a46739741b1a45e5f8dd90edc2fee
FernandaOchoa <monc_sip@hotmail.com>
2019-03-25 02:09:43 -0600

    Js para AoG p2

--- BasicJs/app.js
+++ BasicJs/app.js
@@ -39,3 +39,46 @@ var answer2 = "He is called 'Fernando'"; //Comillas dobles con comillas simples
 console.log(answer2);
 var answer3 = 'He is called "Francisco"'; //Comillas simples con comillas dobles
 console.log(answer3);
+
+//Numeros
+var x1 = 34.00; //Con decimales
+console.log(x1);
+var x2 = 34; // Sin decimales
+console.log(x2);
+
+var y = 123e5; //12300000
+console.log(y);
+var z = 123e-5; // 0.00123
+console.log(z);
+
+// Arrays    0         1      3
+var cars = ["Ford", "Volvo", "BMW"];
+console.log(cars[2]);
+
+// Objetos
+var person = {firstName: "Fer", lastName: "Ochoa", age:25, eyeColor: "Brown"};
+
+// Template literals (${expresion})
+
+let f = 'hola';
+let b = 'mundo';
+console.log('Mi primera aplicacion '+ f +' '+b);
+
+let h = `Mi primera aplicacion ${f} ${b} con template literals`;
+console.log(h);
+
+// Funciones function (parameter1, parameter2){something}
+
+function myFunction(p1, p2){
+  return p1*p2;
+}
+
+var xa  = myFunction(4, 3);
+console.log(xa);
+
+
+function myFunction(a,b){
+  return a-b;
+}
+var xb = myFunction(5, 8);
+console.log(xb);
