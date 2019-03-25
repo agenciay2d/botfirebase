0e16bb71f5ea47fcb1e35636e085cd3b54c20a7f
FernandaOchoa <monc_sip@hotmail.com>
2019-03-25 01:46:27 -0600

    Js para AoG p1

---
+++ BasicJs/app.js
@@ -0,0 +1,41 @@
+// Variables
+var x = 5;
+var y = 6;
+var z = x + y;
+
+console.log("El valor es: "+z);
+
+// Identificadores
+/*
+  Nombres letras, digitos, guiones bajos y  $
+  Comenzar con una letra
+  Pueden comenzar con $ y _
+  Sensibles a mayus y minus
+  No podemos usar como nombre una palabra reservada
+*/
+
+// Tipos de datos
+
+var length = 16; // Numeros
+var lastName = "Johnson"; //Strings
+var x = {firstName: "Fer", lastName: "Ochoa"}; //Object
+
+var a; // Undefined
+a= 5;
+console.log(a); // Numero
+
+a = "Fer";
+console.log(a); //String
+
+//Comillas
+var carName1 = "Volvo XC60"; //Comillas dobles
+console.log(carName1);
+var carName2 = 'Volvo XC60'; //Comillas simples
+console.log(carName2);
+
+var answer1 = "It's alright"; // Comillas dobles con comilla simple
+console.log(answer1);
+var answer2 = "He is called 'Fernando'"; //Comillas dobles con comillas simples
+console.log(answer2);
+var answer3 = 'He is called "Francisco"'; //Comillas simples con comillas dobles
+console.log(answer3);
